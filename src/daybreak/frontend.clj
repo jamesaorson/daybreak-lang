b4eebc7cd8e5bc78bc14bddb99b5741bfd2a1ccb
James Orson <jamesaorson@gmail.com>
2024-04-17 12:50:39 -0700

    Introduce AST stages; Split up message handlers (#4)

--- src/daybreak/frontend.clj
+++ src/daybreak/frontend.clj
@@ -4,47 +4,39 @@
             [daybreak.lex :as lex]
             [net.clojars.jamesaorson.brain.core :as brain]))
 
-(defn make-msg-source [source]
-  {:kind :source
+(defn make-msg-load-source [source]
+  {:kind :load-source
    :source source})
 
-(defn make-msg-lexer-iter [lexer]
-  {:kind :iter-lexer
-   :lexer lexer})
+(defn- -msg-handler [{:keys [data-bag msg msg-handler]}]
+  (match [msg]
+    [{:kind :load-source
+      :source source}] {:running? true
+                        :data-bag (assoc data-bag
+                                         :source source)
+                        :msg-handler lex/msg-handler
+                        :new-msgs [(lex/make-msg-lex (lex/lex source))]}
+    :else {:running? false
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
+           :msg-handler msg-handler}))
 
-(defn make-msg-lexer-done [lexer]
-  {:kind :iter-lexer
-   :lexer lexer})
-
-(defn- msg-handler [{:keys [data-bag msg msg-handler]}]
+(defn- init-msg-handler [{:keys [data-bag msg msg-handler]}]
   (match [msg]
     [[:tick]] (let [{:keys [source]} data-bag]
                 {:running? true
-                 :data-bag data-bag
-                 :msg-handler msg-handler
-                 :new-msgs [(make-msg-source source)]})
-    [{:kind :source
-      :source source}] (let [lexer (lex/lex source)]
-                         {:running? true
-                          :data-bag data-bag
-                          :msg-handler msg-handler
-                          :new-msgs [(make-msg-lexer-iter lexer)]})
-    [{:kind :lexer-iter
-      :lexer lexer}] {:running? false
-                      :data-bag data-bag
-                      :msg-handler msg-handler
-                      :new-msgs [(make-msg-lexer-done lexer)]}
-    [{:kind :lexer-done
-      :lexer _}] {:running? false
-                  :data-bag data-bag
-                  :msg-handler msg-handler}
+                 :data-bag (assoc data-bag
+                                  :source source)
+                 :msg-handler -msg-handler
+                 :new-msgs [(make-msg-load-source source)]})
     :else {:running? false
-           :data-bag (assoc data-bag :unknown-msg msg)
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
            :msg-handler msg-handler}))
 
 (defmulti run (fn [source] (type source)))
 (defmethod run String [source]
-  (brain/spark msg-handler {:source source}))
+  (brain/spark init-msg-handler {:source source}))
 (defmethod run java.io.File [file]
   (run (slurp file)))
 (defmethod run java.nio.file.Path [file]
--- src/daybreak/lex.clj
+++ src/daybreak/lex.clj
@@ -1,5 +1,34 @@
-(ns daybreak.lex)
+(ns daybreak.lex
+  (:require [clojure.core.match :refer [match]]
+            [daybreak.ast :as ast]))
 
 (defn lex
   [input]
   (println "Lexing:\n" input))
+
+(defn make-msg-lex [lexer]
+  {:kind :lex
+   :lexer lexer})
+
+(defn make-msg-lex-done [token-stream]
+  {:kind :lex-done
+   :token-stream token-stream})
+
+(defn msg-handler [{:keys [data-bag msg msg-handler]}]
+  (match [msg]
+    [{:kind :lex
+      :lexer _}] {:running? true
+                  :data-bag data-bag
+                  :msg-handler msg-handler
+                      ;; TODO: Provide a proper token stream
+                  :new-msgs [(make-msg-lex-done [])]}
+    [{:kind :lex-done
+      :token-stream token-stream}] {:running? true
+                                    :data-bag (assoc data-bag
+                                                     :token-stream token-stream)
+                                    :msg-handler ast/msg-handler
+                                    :new-msgs [(ast/make-msg-ast-parse token-stream)]}
+    :else {:running? false
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
+           :msg-handler msg-handler}))
