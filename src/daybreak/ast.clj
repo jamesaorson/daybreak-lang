1bd273156b4314004a55ba1147b94792cb1f1b7c
James Orson <jamesaorson@gmail.com>
2024-04-17 12:47:22 -0700

    ast: Move msg handlers into proper namespaces

--- src/daybreak/ast.clj
+++ src/daybreak/ast.clj
@@ -1,5 +1,31 @@
-(ns daybreak.ast)
+(ns daybreak.ast
+  (:require [clojure.core.match :refer [match]]))
 
-(def DEFAULT
+(def ^:private DEFAULT
   {:kind :ast
    :ast []})
+
+(defn make-msg-ast-parse [token-stream]
+  {:kind :ast-parse
+   :token-stream token-stream})
+
+(defn make-msg-ast-parse-done [ast]
+  {:kind :ast-parse-done
+   :ast ast})
+
+(defn msg-handler [{:keys [data-bag msg msg-handler]}]
+  (match [msg]
+    [{:kind :ast-parse
+      :token-stream _}] {:running? true
+                         :data-bag data-bag
+                         :msg-handler msg-handler
+                         :new-msgs [(make-msg-ast-parse-done DEFAULT)]}
+    [{:kind :ast-parse-done
+      :ast ast}] {:running? false
+                  :data-bag (assoc data-bag
+                                   :ast ast)
+                  :msg-handler msg-handler}
+    :else {:running? false
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
+           :msg-handler msg-handler}))
--- src/daybreak/frontend.clj
+++ src/daybreak/frontend.clj
@@ -1,82 +1,37 @@
 (ns daybreak.frontend
   (:require [babashka.fs :as fs]
             [clojure.core.match :refer [match]]
-            [daybreak.ast :as ast]
             [daybreak.lex :as lex]
             [net.clojars.jamesaorson.brain.core :as brain]))
 
-(defn make-msg-ast-parse [token-stream]
-  {:kind :ast-parse
-   :token-stream token-stream})
-
-(defn make-msg-ast-parse-done [ast]
-  {:kind :ast-parse-done
-   :ast ast})
-
-(defn make-msg-source [source]
-  {:kind :source
+(defn make-msg-load-source [source]
+  {:kind :load-source
    :source source})
 
-(defn make-msg-lex [lexer]
-  {:kind :lex
-   :lexer lexer})
-
-(defn make-msg-lex-done [token-stream]
-  {:kind :lex-done
-   :token-stream token-stream})
-
-(defn- ast-msg-handler [{:keys [data-bag msg msg-handler]}]
+(defn- -msg-handler [{:keys [data-bag msg msg-handler]}]
   (match [msg]
-    [{:kind :ast-parse
-      :ast ast}] {:running? true
-                  :data-bag (assoc data-bag :ast ast)
-                  :msg-handler msg-handler
-                  :new-msgs [(make-msg-ast-parse-done ast)]}
-    [{:kind :ast-parse-done
-      :ast ast}] {:running? false
-                  :data-bag (assoc data-bag :ast ast)
-                  :msg-handler msg-handler}
-    :else {:running? false
-           :data-bag (assoc data-bag :unknown-msg msg)
-           :msg-handler msg-handler}))
-
-(defn- lex-msg-handler [{:keys [data-bag msg msg-handler]}]
-  (match [msg]
-    [{:kind :lex
-      :lexer _}] {:running? true
-                  :data-bag data-bag
-                  :msg-handler msg-handler
-                      ;; TODO: Provide a proper token stream
-                  :new-msgs [(make-msg-lex-done [{}])]}
-    [{:kind :lex-done
-      :token-stream token-stream}] {:running? true
-                                    :data-bag data-bag
-                                    :msg-handler ast-msg-handler
-                                    :new-msgs [(make-msg-ast-parse token-stream)]}
-    :else {:running? false
-           :data-bag (assoc data-bag :unknown-msg msg)
-           :msg-handler msg-handler}))
-
-(defn- source-msg-handler [{:keys [data-bag msg msg-handler]}]
-  (match [msg]
-    [{:kind :source
+    [{:kind :load-source
       :source source}] {:running? true
-                        :data-bag data-bag
-                        :msg-handler lex-msg-handler
-                        :new-msgs [(make-msg-lex (lex/lex source))]}
+                        :data-bag (assoc data-bag
+                                         :source source)
+                        :msg-handler lex/msg-handler
+                        :new-msgs [(lex/make-msg-lex (lex/lex source))]}
     :else {:running? false
-           :data-bag (assoc data-bag :unknown-msg msg)
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
            :msg-handler msg-handler}))
 
 (defn- init-msg-handler [{:keys [data-bag msg msg-handler]}]
   (match [msg]
     [[:tick]] (let [{:keys [source]} data-bag]
                 {:running? true
-                 :data-bag data-bag
-                 :msg-handler source-msg-handler
-                 :new-msgs [(make-msg-source source)]})
+                 :data-bag (assoc data-bag
+                                  :source source)
+                 :msg-handler -msg-handler
+                 :new-msgs [(make-msg-load-source source)]})
     :else {:running? false
-           :data-bag (assoc data-bag :unknown-msg msg)
+           :data-bag (assoc data-bag
+                            :unknown-msg msg)
            :msg-handler msg-handler}))
 
 (defmulti run (fn [source] (type source)))
