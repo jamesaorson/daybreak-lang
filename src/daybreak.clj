ddd714680f1066b0b5cad38d24cd594d7504f2d6
James Orson <jamesaorson@gmail.com>
2024-04-17 11:34:00 -0700

    clojure: Reformat arg parsing

--- src/daybreak.clj
+++ src/daybreak.clj
@@ -1,18 +1,19 @@
 #!/usr/bin/env bb
   (ns daybreak
     (:require [babashka.fs :as fs]
-              [daybreak.frontend :as frontend]
-              [daybreak.programs :as programs]))
+              [daybreak.frontend :as frontend]))
+
+(defn- test []
+  (let [examples (fs/list-dir "examples")]
+    (doseq [example examples]
+      (let [files (fs/list-dir example)]
+        (doseq [file files]
+          (frontend/run file))))))
 
 (defn -main [& args]
-  (if (some #(= % "--only-download-deps") args)
-    (System/exit 0)
-    (when (some #(= % "--test") args)
-      (let [examples (fs/list-dir "examples")]
-        (doseq [example examples]
-          (let [files (fs/list-dir example)]
-            (doseq [file files]
-              (frontend/run file))))))))
+  (cond
+    (some #(= % "--only-download-deps") args) (System/exit 0)
+    (some #(= % "--test") args) (test)))
 
 (when (= *file* (System/getProperty "babashka.file"))
   (-main *command-line-args*))
